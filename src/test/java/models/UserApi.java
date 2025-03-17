72f8e27ed52dbe658b1d6a6e808e22145e84dffd
Evgeniy Popov <armedlemon@yandex.ru>
2025-03-17 21:07:55 +0300

    First_commit_diplom_3_v2_browFac_Api

---
+++ src/test/java/models/UserApi.java
@@ -0,0 +1,36 @@
+package models;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class UserApi {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/api/auth/";
+
+    public Response registerUser(UserModel user) {
+        return RestAssured.given()
+                .contentType("application/json")
+                .body(user)
+                .post(BASE_URL + "register");
+    }
+
+    public Response loginUser(UserModel user) {
+        return RestAssured.given()
+                .contentType("application/json")
+                .body(user)
+                .post(BASE_URL + "login");
+    }
+
+    public Response updateUser(String token, UserModel user) {
+        return RestAssured.given()
+                .contentType("application/json")
+                .auth().oauth2(token)
+                .body(user)
+                .patch(BASE_URL + "user");
+    }
+
+    public Response deleteUser(String token) {
+        return RestAssured.given()
+                .auth().oauth2(token)
+                .delete(BASE_URL + "user");
+    }
+}
